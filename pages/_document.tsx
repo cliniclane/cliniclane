2f462062166e50e03792675c6f916722ab4537fe
Siddhartha Roy <sid86harth@gmail.com>
2025-02-23 06:26:46 +0530

    feat: meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="fast2sms" content="HRxutiTLhVsWhWhghyCK0GPMNPC1WkCU" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
