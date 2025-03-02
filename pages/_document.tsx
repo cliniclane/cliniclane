59f22591930fa400065949d392f8561125ee7012
Siddhartha Roy <sid86harth@gmail.com>
2025-03-02 20:36:57 +0530

    rafce: Google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -38,6 +39,22 @@ export default function Document() {
         />
         <meta name="Cliniclane" content="&nbsp;" />
         <meta name="msapplication-TileColor" content="#FFFFFF" />
+
+        {/* Google Tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JZ1KW909QN"
+        ></Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JZ1KW909QN');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
