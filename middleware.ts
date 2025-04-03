da23106d746e9279390a3ba5108a7e8f5a93a965
Siddhartha Roy <sid86harth@gmail.com>
2025-04-03 20:47:27 +0530

    feat: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/admin/article/:path*", "/admin", "/admin/pages/:path*", "/admin/users"],
+  matcher: [
+    "/admin/article/:path*",
+    "/admin",
+    "/admin/pages/:path*",
+    "/admin/users",
+    "admin/languages",
+  ],
 };
