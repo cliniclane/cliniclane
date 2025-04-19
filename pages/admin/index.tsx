856d79902c47a9cc07296ab79118fd4a891c646f
Siddhartha Roy <sid86harth@gmail.com>
2025-04-20 03:21:58 +0530

    fix: enhance FAQ formatting for improved readability

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -145,7 +145,7 @@ export default function Articles() {
       return faqs
         .map(
           (faq) =>
-            `__Q: ${faq.question}__\n**A:** ${faq.answer}`
+            `__Q: ${faq.question}__\n\n**A:** ${faq.answer}`
         )
         .join("\n\n");
     }
